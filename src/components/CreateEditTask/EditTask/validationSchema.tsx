69183aabfe9986736698ddd804a8c59f804e6a0c
Kcepriu <kcepriu@gmail.com>
2024-03-30 12:14:04 +0200

    Change count char in name task

--- src/components/CreateEditTask/EditTask/validationSchema.tsx
+++ src/components/CreateEditTask/EditTask/validationSchema.tsx
@@ -5,7 +5,7 @@ export const validationSchema = Yup.object({
   id: Yup.number().required("Id is required"),
 
   name: Yup.string()
-    .max(20, "Must be 20 characters or less")
+    .max(50, "Must be 50 characters or less")
     .required("Name is required"),
 
   description: Yup.string()
